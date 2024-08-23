e65461c6624f7f0d1637b98311c525de9a0f4cce
pratikgrv <pratikgaurav37@gmail.com>
2024-08-24 00:55:36 +0530

    ui :zap:: contentlayer init

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,68 +1,43 @@
-import { cn } from '@/lib/utils';
-import { DiscIcon } from 'lucide-react';
-import Link from 'next/link';
-import React from 'react'
-import { MainNav } from './main-nav';
+import Link from "next/link";
+import React from "react";
+
+import { cn } from "@/lib/utils";
+
+import { MainNav } from "@/components/main-nav";
+import { DiscordSVG, GithubSVG, TwitterSVG } from "@/components/icons/svgs";
+import { MobileNav } from "@/components/mobile-nav";
 
 const SiteHeader = () => {
-  return (
+	return (
 		<header
 			className={cn(
 				"supports-backdrop-blur:bg-background/90 sticky top-0 z-40 w-full bg-background/40 backdrop-blur-lg"
 			)}
 		>
 			<div className="container flex h-16 items-center">
-				 <MainNav />
-			{/*	<MobileNav /> */}
+				<MainNav />
+				<MobileNav />
 				<div className="flex flex-1 items-center justify-between gap-2 md:justify-end">
-					
-
 					<div className="w-full flex-1 md:w-auto md:flex-none">
 						{/* <CommandMenu /> */}
+						search
 					</div>
 					<nav className="flex items-center gap-1">
-						<Link
-							href=""
-							target="_blank"
-							rel="noreferrer"
-						>
-							<div
-								className={cn(
-								
-									"w-9 px-0"
-								)}
-							>
-								<DiscIcon className="size-4" />
+						<Link href="" target="_blank" rel="noreferrer">
+							<div className={cn("w-9 px-0")}>
+								<DiscordSVG className="size-4" />
 								<span className="sr-only">Discord</span>
 							</div>
 						</Link>
-						<Link
-							href=""
-							target="_blank"
-							rel="noreferrer"
-						>
-							<div
-								className={cn(
-									
-									"w-9 px-0"
-								)}
-							>
-								{/* <Icons.gitHub className="size-4" /> */}g
+						<Link href="" target="_blank" rel="noreferrer">
+							<div className={cn("w-9 px-0")}>
+								<GithubSVG className=" size-4" />
 								<span className="sr-only">GitHub</span>
 							</div>
 						</Link>
-						<Link
-							href=""
-							target="_blank"
-							rel="noreferrer"
-						>
-							<div
-								className={cn(
-									
-									"w-9 px-0"
-								)}
-							>
-								{/* <Icons.twitter className="size-4 fill-current" /> */}t
+						<Link href="" target="_blank" rel="noreferrer">
+							<div className={cn("w-9 px-0")}>
+								<TwitterSVG className="size-4" />
 								<span className="sr-only">Twitter</span>
 							</div>
 						</Link>
@@ -73,6 +48,6 @@ const SiteHeader = () => {
 			<hr className="m-0 h-px w-full border-none bg-gradient-to-r from-neutral-200/0 via-neutral-200/30 to-neutral-200/0" />
 		</header>
 	);
-}
+};
 
-export default SiteHeader
+export default SiteHeader;
